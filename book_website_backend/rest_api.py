fe57a853d5193a434c82c98cb8a025c45bbb102c
george ward <gcward18@gmail.com>
2019-11-15 12:05:33 -0600

    have images of authors loaded

--- book_website_backend/rest_api.py
+++ book_website_backend/rest_api.py
@@ -3,7 +3,7 @@ from flask import Flask
 from flask_restful import Api, Resource, reqparse
 from flask_cors import CORS
 from connect import Connect
-import os
+import os, pandas
 
 app = Flask(__name__)
 api = Api(app)
@@ -20,8 +20,8 @@ class Author(Resource):
                     'scsp.mst.edu'
                 )
         conn.establish_connection()
-        data = conn.get_query_data('SELECT * FROM CAO346.AUTHORS')
-        
+        data = conn.get_query_data('SELECT * FROM GCWZF4.AUTHORS')
+
         authors = []
         for row in data:
             authors.append({"name":row[0]})
@@ -46,7 +46,7 @@ class BookEditions(Resource):
                     'scsp.mst.edu'
                 )
         conn.establish_connection()
-        data = conn.get_query_data('SELECT * FROM CAO346.BOOK_EDITIONS')
+        data = conn.get_query_data('SELECT TITLE, EDITION, AUTHORS.AUTHOR, PUBLISH_YEAR, IMAGE_PATH FROM GCWZF4.BOOK_EDITIONS JOIN GCWZF4.AUTHORS ON AUTHORS.AUTHOR = BOOK_EDITIONS.AUTHOR')
         
         books = []
         i = 0
@@ -54,13 +54,15 @@ class BookEditions(Resource):
             books.append({
                 "id": i,
                 "title":row[0],
-                "author": row[-2],
-                "year": row[4]
+                "edition":row[1],
+                "author": row[2],
+                "year": row[3],
+                "image_path": row[4]
                 })
             i+=1
         conn.close_connection()
 
-        return books, 200
+        return books[:20], 200
 
     def post(self, name):
         pass
