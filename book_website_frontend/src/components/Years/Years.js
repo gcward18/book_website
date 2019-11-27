7e772787523e8b45cd3e9ca173407e546745099e
lmalloy <ldm437@mst.edu>
2019-11-26 19:13:35 -0600

    adding Years shell

---
+++ book_website_frontend/src/components/Years/Years.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Years extends Component {
+    render() {
+        return (
+            <div>
+                years goes heres
+            </div>
+        )
+    }
+}
