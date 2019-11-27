ef0a94a4f4adc78919b67e70d3a8a5f69a51eb1f
Skylar <stbrb@mst.edu>
2019-11-26 19:57:13 -0600

    add page shell

---
+++ book_website_frontend/src/components/Add/add.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class add extends Component {
+    render() {
+        return (
+            <div>
+               I hate REACT! Thanks George!
+            </div>
+        )
+    }
+}
