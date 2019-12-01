09ef1e6ac7e9d22f3d5cfec11235d8ab07496e08
lmalloy <ldm437@mst.edu>
2019-12-01 01:23:44 -0600

    adding books and styling -- years font color not changing in h1

--- book_website_frontend/src/components/Years/Years.js
+++ book_website_frontend/src/components/Years/Years.js
@@ -1,11 +1,66 @@
 import React, { Component } from 'react'
+import '../Years/Years.css';
 
 export default class Years extends Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            error : null,
+            isLoaded : false,
+            data : []
+        };
+    }
+
+    componentDidMount(){
+        fetch(
+            'http://localhost:5000/years',
+            {
+                mode:'cors',
+                headers: {
+                    'Access-Control-Allow-Origin': 'http://localhost:5000'
+                }
+            }
+        )
+        .then(res => {res.json()
+        .then(data => {
+                this.setState({data:data.publish_year, isLoaded:true});
+                console.log("state", this.state.data)
+            });
+        })
+    }
+
     render() {
-        return (
+        const { error, isLoaded, data } = this.state;
+        var years = Object.keys(data)
+        const books = years.map(year =>
             <div>
-                years goes heres
+                <h1>{year}</h1>
+                {
+                    data[year].map(title => 
+                        <div>
+                            <h2>{title}</h2> 
+                        </div>)
+                }
+                <h3></h3>
             </div>
         )
+
+        /*var arr = [];
+        Object.keys(data).forEach(function(publish_year){
+            arr.push(data[publish_year]);
+            console.log(publish_year)
+            console.log(data[publish_year])
+        });*/
+
+        return (
+            <div className="wrapper" >
+                <div className="body-wrapper">
+                    {
+                        books
+                    }               
+                </div>
+            </div>
+        );
     }
 }
